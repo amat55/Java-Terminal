650fb768b80d26b95ead61cb57ea15d870dd4172
ahmet <41929175+amat55@users.noreply.github.com>
2021-11-07 17:30:27 +0100

    Udemy challenge

---
+++ src/com/company/FifteenTask.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class FifteenTask {
+    //Udemy challenge :)
+    public static void main(String[] args) {
+    printMegaBytesAndKiloBytes(5);
+    }
+    public static void printMegaBytesAndKiloBytes(int kiloBytes){
+        if(kiloBytes<0){
+            System.out.println("Invalid Value");
+        }
+        else{
+
+            System.out.println(kiloBytes + "KB =" + (kiloBytes/1024) + " MB " + (kiloBytes%1024) + " KB " );
+        }
+    }
+}
---
+++ src/com/company/SixtheenTask.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class SixtheenTask {
+    //Udemy challenge :)
+    public static void main(String[] args) {
+
+    }
+    public static boolean shouldWakeUp(boolean barking,int hourOfDay){
+        if(!barking || hourOfDay < 0 || hourOfDay > 23 || (hourOfDay > 7 && hourOfDay < 23)){
+            return false;
+        }
+        return true;
+    }
+}
