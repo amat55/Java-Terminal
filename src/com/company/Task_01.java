4bac5a9be177eed51c25dd2df1bbf020a89e0db6
ahmet <41929175+amat55@users.noreply.github.com>
2021-11-09 01:12:13 +0100

    New Challenges

--- src/com/company/Task_01.java
+++ src/com/company/Task_01.java
@@ -1,6 +1,6 @@
 package com.company;
 import java.util.Scanner;
-public class Main_01 {
+public class Task_01 {
     public static void main(String[] args) {
         int number = 60;
         System.out.print("Factors of " + number + " are: ");
---
+++ src/com/company/Task_21.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Task_21 {
+    //I'll finish tomorrow :)
+    //Udemy Challenge
+    public static void main(String[] args) {
+        isLeapYear(-99);
+
+    }
+
+    public static boolean isLeapYear(int year){
+        if(year>=0 && year<=9999){
+            System.out.println("sss" +year);
+            return false;
+
+        }else {
+            System.out.println("zzz" +year);
+            return true;
+        }
+
+
+    }
+}
