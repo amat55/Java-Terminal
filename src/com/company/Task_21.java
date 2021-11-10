cc7dc3fb6bc129d563d4463cbe127d88c75001d4
ahmet <41929175+amat55@users.noreply.github.com>
2021-11-10 16:24:50 +0100

    New Challenges

--- src/com/company/Task_21.java
+++ src/com/company/Task_21.java
@@ -4,20 +4,41 @@ public class Task_21 {
     //I'll finish tomorrow :)
     //Udemy Challenge
     public static void main(String[] args) {
-        isLeapYear(-99);
+        //isLeapYear(-99);
 
     }
 
     public static boolean isLeapYear(int year){
-        if(year>=0 && year<=9999){
-            System.out.println("sss" +year);
-            return false;
-
-        }else {
-            System.out.println("zzz" +year);
-            return true;
+        if(year>=1 && year <=9999){
+            if((year%4==0 && year %100 !=0)|| year%400 ==0){
+                return true;
+            }
         }
+        return false;
+    }
+    public static int getDaysInMonth(int month, int year){
+        boolean leapyear = isLeapYear(year);
+        if ((month <1 || month >12) || (year <1 || year >9999)){
+            return -1;
+        }
+        else {
+            switch (month){
+                case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                    return 31;
+                case 2:
+                    if (leapyear){
+                        return 29;
+                    }
+                    else{
+                        return 28;
+                    }
+                case 4: case 6: case 9: case 11:
+                    return 30;
+                default:
+                    return -1;
 
+            }
+        }
 
     }
 }
---
+++ src/com/company/Task_22.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Task_22 {
+    public static void main(String[] args) {
+        int[] a =new int[3];
+        System.out.println(a);
+
+    }
+}
