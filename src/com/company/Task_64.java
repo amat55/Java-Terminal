a78aec62e832cf0785830b8e1a78337af4408ad1
ahmet <41929175+amat55@users.noreply.github.com>
2021-12-05 18:04:02 +0100

    Udemy Challenges

---
+++ src/com/company/Task_64.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Task_64 {
+    public static void main(String[] args) {
+
+        Task_64_2 calculator = new Task_64_2();
+        calculator.setFirstNumber(5.0);
+        calculator.setSecondNumber(4);
+        System.out.println("add= " + calculator.getAdditionResult());
+        System.out.println("subtract= " + calculator.getSubtractionResult());
+        calculator.setFirstNumber(5.25);
+        calculator.setSecondNumber(0);
+        System.out.println("multiply= " + calculator.getMultiplicationResult());
+        System.out.println("divide= " + calculator.getDivisionResult());
+    }
+}
